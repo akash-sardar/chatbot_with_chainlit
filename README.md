870a9cd3a14141b88b152115c577cb858a2d9e5d
Akash-Sardar <lordakashjoy@gmail.com>
2024-08-01 00:30:00 -0400

    Project complete

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# chatbot_with_chainlit
-Chatbot with Chainlit UI
+# CHATBOT
+Application to create and run a food ordering chatbot that suggests food recommendations
+## Tools used:
+* CHAINLIT UI
+* LLM : OpenAI GPT 4o Mini gpt-4o-mini
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "food_chatbot",
+    version = "0.0.0",
+    author="Akash Sardar",
+    author_email="akashjoy2023@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
